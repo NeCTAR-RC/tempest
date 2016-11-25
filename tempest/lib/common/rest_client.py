d308ecd50acc902900898521a9665b91837eab7f
Sam Morrison <sorrison@gmail.com>
2016-11-25 14:19:53 +1100

    NeCTARism: Pass availability param through to volumes client
    Change-Id: I354ffd20f6b8a59304a7227e75e9d637987b5052

--- tempest/lib/common/rest_client.py
+++ tempest/lib/common/rest_client.py
@@ -84,7 +84,9 @@ class RestClient(object):
                  build_interval=1, build_timeout=60,
                  disable_ssl_certificate_validation=False, ca_certs=None,
                  trace_requests='', name=None, http_timeout=None,
-                 proxy_url=None, follow_redirects=True):
+                 proxy_url=None, follow_redirects=True,
+                 availability_zone=None):
+
         self.auth_provider = auth_provider
         self.service = service
         self.region = region
@@ -93,6 +95,7 @@ class RestClient(object):
         self.build_interval = build_interval
         self.build_timeout = build_timeout
         self.trace_requests = trace_requests
+        self.availability_zone = availability_zone
 
         self._skip_path = False
         self.general_header_lc = set(('cache-control', 'connection',
