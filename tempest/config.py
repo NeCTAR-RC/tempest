7405a80c0439f538f37ae96006e4d58e03f29a30
Sam Morrison <sorrison@gmail.com>
2016-11-25 16:08:34 +1100

    NeCTARism: Pass availability param through to servers client
    Change-Id: I71799e3a38e1b4aa637eac39f144bc99468c6462

--- tempest/config.py
+++ tempest/config.py
@@ -331,7 +331,11 @@ ComputeGroup = [
                     "min_microversion and max_microversion. "
                     "If both values are not specified, Tempest avoids tests "
                     "which require a microversion. Valid values are string "
-                    "with format 'X.Y' or string 'latest'"),
+                    "with format 'X.Y' or string 'latest'",
+                    deprecated_group='compute-feature-enabled'),
+    cfg.StrOpt('availability_zone',
+               default=None,
+               help="Availability zone to be used in tests."),
 ]
 
 compute_features_group = cfg.OptGroup(name='compute-feature-enabled',
