89c44edc9d6c13b99024c5c4ab4d1ead5c379fc6
rocky <shi.yan@unimelb.edu.au>
2017-07-21 07:28:57 +0000

    Change to non-admin user for tempest network scneario tests
    Change-Id: If92dcd23ede95cc98b66d229c8fcd988de18e5b3

--- tempest/scenario/manager.py
+++ tempest/scenario/manager.py
@@ -688,7 +688,7 @@ class NetworkScenarioTest(ScenarioTest):
 
     """
 
-    credentials = ['primary', 'admin']
+    credentials = ['primary']
 
     @classmethod
     def skip_checks(cls):
@@ -737,7 +737,7 @@ class NetworkScenarioTest(ScenarioTest):
             :returns: True if subnet with cidr already exist in tenant
                   False else
             """
-            cidr_in_use = self.os_admin.subnets_client.list_subnets(
+            cidr_in_use = self.os_primary.subnets_client.list_subnets(
                 tenant_id=tenant_id, cidr=cidr)['subnets']
             return len(cidr_in_use) != 0
 
@@ -786,7 +786,7 @@ class NetworkScenarioTest(ScenarioTest):
         return subnet
 
     def _get_server_port_id_and_ip4(self, server, ip_addr=None):
-        ports = self.os_admin.ports_client.list_ports(
+        ports = self.os_primary.ports_client.list_ports(
             device_id=server['id'], fixed_ip=ip_addr)['ports']
         # A port can have more than one IP address in some cases.
         # If the network is dual-stack (IPv4 + IPv6), this port is associated
@@ -816,7 +816,7 @@ class NetworkScenarioTest(ScenarioTest):
         return port_map[0]
 
     def _get_network_by_name(self, network_name):
-        net = self.os_admin.networks_client.list_networks(
+        net = self.os_primary.networks_client.list_networks(
             name=network_name)['networks']
         self.assertNotEmpty(net,
                             "Unable to get network by name: %s" % network_name)
